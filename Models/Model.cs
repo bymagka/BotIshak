fe1296a05bfd0d63b515c61249b36fc7ad3f4235
bymagka <bymagka@bk.ru>
2021-06-24 10:20:59 +0300

    restructurization

---
+++ Models/Model.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using OV2Bot;
+using System.Text.RegularExpressions;
+using BotSQLWrapper;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace BotModel
+{
+   
+    public enum UserStates{
+        Authorized,
+        NonAuthorized,
+        AuthorizationInProgress,
+        CreatingBase
+    }
+
+    public static class Model
+    {
+        public static DateTime startTime { get; set; }
+        static Regex regexPattern { get; set; }
+        static string serverAddress { get; } = @"Srvr=""slocalhost:1841"";Ref="; 
+
+        public static OV2BotClient botClient { get; set; }
+
+        public static void OnMessage(object s, Telegram.Bot.Args.MessageEventArgs arg)
+        {
+            
+            // skip old messages
+            if (arg.Message.Date < startTime || arg.Message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
+                return;
+
+            string msgText = arg.Message.Text;
+            string replyMsg = String.Empty;
+
+
+            UserInfo userInfo = new UserInfo
+            {
+                Id = arg.Message.From.Id,
+                Username = arg.Message.From.Username,
+                ChatId = arg.Message.Chat.Id,
+                MessageId = arg.Message.MessageId
+
+            };
+
+
+            var activeUser = botClient.ActiveUsers
+                         .Select((x) => x)
+                         .Where((x) => x.Id == userInfo.Id)
+                         .FirstOrDefault();
+
+            if (activeUser == null || activeUser.State == UserStates.NonAuthorized || activeUser.State == UserStates.AuthorizationInProgress)
+            {
+
+                AuthorizateUser(ref activeUser,userInfo, msgText);
+            }
+            else
+            {
+                CommandHandler(ref activeUser,msgText,userInfo);
+            }
+        }
+
+        private static void AuthorizateUser(ref BotUser activeUser, UserInfo userInfo,string message)
+        {
+            
+
+            if (activeUser == null || activeUser.State == UserStates.NonAuthorized)
+            {
+                activeUser = new BotUser(userInfo.Id, userInfo.ChatId, userInfo.Username);
+                botClient.ActiveUsers.Add(activeUser);
+                activeUser.State = UserStates.AuthorizationInProgress;
+
+                botClient.SendTextMessageAsync(
+                    chatId: userInfo.ChatId,
+                    text: $"Log in please. You should enter your login and password with the space between",
+                    replyToMessageId: userInfo.MessageId);
+
+             
+            }
+            else 
+            {
+                    regexPattern = new Regex(@"^[^\s]+\s+[^\s]+$");
+                if (regexPattern.IsMatch(message))
+                {
+                    string[] authData = message.Split(' ');
+                    string authUserName = authData[0];
+                    string authUserPassword = authData[1];
+                    if (CheckAuthentificationData(authUserName, authUserPassword))
+                    {
+                        botClient.SendTextMessageAsync(
+                                 chatId: userInfo.ChatId,
+                                 text: $"Log in successfully. Great! Now you can use bot's commands. Type /help to get all commands list",
+                                 replyToMessageId: userInfo.MessageId);
+
+                        activeUser.UsernameRDP = authUserName;
+                        activeUser.State = UserStates.Authorized;
+
+                    }
+                    else
+                    {
+                        botClient.SendTextMessageAsync(
+                             chatId: userInfo.ChatId,
+                             text: $"Uncorrect user or password. Try again",
+                             replyToMessageId: userInfo.MessageId);
+
+                        activeUser.State = UserStates.AuthorizationInProgress;
+                    }
+                }
+                else
+                {
+                    botClient.SendTextMessageAsync(
+                        chatId: userInfo.ChatId,
+                        text: $"You entered not the valid authentification data. Repeat , please. Input your login and password with the space between",
+                         replyToMessageId: userInfo.MessageId);
+                }
+                        
+            }
+        }
+
+        private static bool CheckAuthentificationData(string authUserName, string authUserPassword)
+        {
+
+            return SQLWrapper.CheckAuthorizationData(authUserName,authUserPassword);
+        }
+
+        private static void CommandHandler(ref BotUser activeUser, string msgText,UserInfo userInfo)
+        {
+            switch (msgText.ToLower())
+            {
+                case "/createdatabase":
+                    PrepareForCreatingDatabase(ref activeUser, userInfo);
+                    break;
+                default:
+                    SimpleTextMessageHandler(ref activeUser, userInfo,msgText);
+                    break;
+            }
+        }
+
+        static void PrepareForCreatingDatabase(ref BotUser activeUser, UserInfo userInfo)
+        {
+            botClient.SendTextMessageAsync(
+                chatId: userInfo.ChatId,
+                text: $"Ok, i can create database for you. But you should input the database name. It must contains only latin symbols and digits.",
+                replyToMessageId: userInfo.MessageId);
+
+            activeUser.State = UserStates.CreatingBase;
+        }
+
+        private static void SimpleTextMessageHandler(ref BotUser activeUser, UserInfo userInfo, string msgText)
+        {
+            if(activeUser.State == UserStates.CreatingBase)
+            {
+                regexPattern = new Regex(@"^[a-zA-Z0-9]+$");
+                if (!regexPattern.IsMatch(msgText))
+                {
+                    botClient.SendTextMessageAsync(
+                          chatId: userInfo.ChatId,
+                          text: $"Database name isn't valid. Please check and try again.",
+                          replyToMessageId: userInfo.MessageId);
+                }
+                else 
+                {
+                    //todo: creating base
+                    string baseName = $"{activeUser.UsernameRDP}_{msgText}";
+                    if (CreateDatabase(baseName))
+                    {
+                        botClient.SendTextMessageAsync(
+                              chatId: userInfo.ChatId,
+                              text: $"Base was successfully added at this address:\n {serverAddress}\"{baseName}\"",
+                              replyToMessageId: userInfo.MessageId);
+                    }
+                    else
+                    {
+                        botClient.SendTextMessageAsync(
+                              chatId: userInfo.ChatId,
+                              text: $"Whoops... Something went wrong. Send a letter to administator. Maybe he knows",
+                              replyToMessageId: userInfo.MessageId);
+                    };
+
+                    activeUser.State = UserStates.Authorized;
+                }
+            }
+            else
+            {
+                botClient.SendTextMessageAsync(
+                      chatId: userInfo.ChatId,
+                      text: $"I don't know this command. Don't be mad, please.");
+            }
+        }
+
+        private static Boolean CreateDatabase(string dataBaseName)
+        {
+            try
+            {
+                Process executorProcess = Process.Start($@"{Directory.GetCurrentDirectory()}\Executor\bin\executor.cmd", $@"-s ""Scripts\CreateDatabase.sbsl"" {dataBaseName}");
+                //why do this process always finish with exit code 255? At the working server always breaks with exception??
+                //if (executorProcess is null || executorProcess.ExitCode == 255)
+                //{
+                //    return false;
+                //}
+                return true;
+            }
+            catch
+            {
+                return true;
+            }
+
+        }
+    }
+
+
+
+    public class BotUser
+    {
+        public UserStates State { get; set; }
+        public long Id { get; set; }
+        public long ChatId { get; set; }
+        public string Username { get; set; }
+        public string UsernameRDP { get; set; }
+
+        public BotUser(long Id, long ChatId, string UserName = "")
+        {
+            this.Id = Id;
+            this.ChatId = ChatId;
+            this.Username = UserName;
+            State = UserStates.NonAuthorized;
+        }
+    }
+
+    struct UserInfo
+    {
+        public long Id { get; set; }
+        public long ChatId { get; set; }
+        public string Username { get; set; }
+        public int  MessageId { get; set; }
+        
+    }
+}
---
+++ Models/SQLWrapper.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotSQLWrapper
+{
+    public static class SQLWrapper
+    {
+        static SqlDataAdapter dataAdapter = new SqlDataAdapter();
+        
+        static string database = @"Server=WIN-9PP8GDINMAP;Database=Users;User Id = admin; Password=1C,e[extn;";
+
+        public static bool CheckAuthorizationData(string userName, string password)
+        {
+            
+            DataTable dt = new DataTable("Users");
+            using (SqlConnection sqlConnection = new SqlConnection(database))
+            {
+               
+                string selectQueryUsers = @"SELECT Name,Password FROM Users WHERE Name = @Name AND Password = @Password";
+
+                SqlCommand checkAuthorization = new SqlCommand(selectQueryUsers, sqlConnection);
+
+                SqlParameter sqlParameterName = new SqlParameter("Name", userName);
+                SqlParameter sqlParameterPassword = new SqlParameter("Password", password);
+
+                checkAuthorization.Parameters.Add(sqlParameterName);
+                checkAuthorization.Parameters.Add(sqlParameterPassword);
+
+                dataAdapter.SelectCommand = checkAuthorization;
+                dataAdapter.Fill(dt);
+            }
+
+            return dt.Rows.Count != 0;
+        }
+
+        public static DataTable GetUsers()
+        {
+
+            DataTable dt = new DataTable("Users");
+            using (SqlConnection sqlConnection = new SqlConnection(database))
+            {
+
+                string selectQuery = "SELECT * FROM Users";
+
+                dataAdapter.SelectCommand = new SqlCommand(selectQuery, sqlConnection);
+
+
+                dataAdapter.Fill(dt);
+
+            }
+
+            return dt;
+        }
+
+        public static void AddUsers(DataTable dtUsers)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(database))
+            {
+                //insert
+                string insertQueryUsers = @"INSERT INTO Users (Name,Password) VALUES (@Name,@Password); SET @ID = @@IDENTITY";
+
+                SqlCommand insertUsers = new SqlCommand(insertQueryUsers, sqlConnection);
+                insertUsers.Parameters.Add("@Name", SqlDbType.NVarChar, -1, "Name");
+                insertUsers.Parameters.Add("@Password", SqlDbType.NVarChar, -1, "Password");
+                insertUsers.Parameters.Add("@Id", SqlDbType.Int, 0, "Id");
+
+                dataAdapter.InsertCommand = insertUsers;
+
+                dataAdapter.Update(dtUsers);
+            }
+        }
+
+        public static DataTable GetBases()
+        {
+
+            DataTable dt = new DataTable("Bases");
+            using (SqlConnection sqlConnection = new SqlConnection(database))
+            {
+
+                string selectQuery = "SELECT * FROM Bases";
+
+                dataAdapter.SelectCommand = new SqlCommand(selectQuery, sqlConnection);
+
+
+                dataAdapter.Fill(dt);
+
+            }
+
+            return dt;
+        }
+
+        public static void UpdateUsers(DataTable dtUsers)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(database))
+            {
+
+                //insert
+                string updateQueryUsers = @"UPDATE Users SET Name = @Name, Password = @Password WHERE Id = @Id";
+
+                SqlCommand updateUsers = new SqlCommand(updateQueryUsers, sqlConnection);
+                updateUsers.Parameters.Add("@Name", SqlDbType.NVarChar, -1, "Name");
+                updateUsers.Parameters.Add("@Password", SqlDbType.NVarChar, -1, "Password");
+                updateUsers.Parameters.Add("@Id", SqlDbType.Int, 0, "Id");
+
+                dataAdapter.InsertCommand = updateUsers;
+
+                dataAdapter.Update(dtUsers);
+            }
+        }
+    }
+}
+
+
