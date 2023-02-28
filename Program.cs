4f0af279ed74ade4f4ab85070583140e23ebd37a
Alexandr Tinin <AUTinin@grotem.com>
2023-02-28 22:08:20 +0300

    commit from test_2 branch

--- Program.cs
+++ Program.cs
@@ -15,16 +15,19 @@ namespace OV2Bot
     {
         static void Main(string[] args)
         {
-            string token = "1053504808:AAFK_U57vMVSMF69RTdrHmaVz8-7wbV3gNo";
-            OV2BotClient OV2Bot = new OV2BotClient(token);
+            Console.WriteLine("Change from test_2 branch");
 
-            OV2Bot.OnMessage += Model.OnMessage;
-
-            Model.startTime = DateTime.UtcNow;
-            Model.botClient = OV2Bot;
-            OV2Bot.StartReceiving();
-           
             Console.ReadLine();
+            //string token = "1053504808:AAFK_U57vMVSMF69RTdrHmaVz8-7wbV3gNo";
+            //OV2BotClient OV2Bot = new OV2BotClient(token);
+
+            //OV2Bot.OnMessage += Model.OnMessage;
+
+            //Model.startTime = DateTime.UtcNow;
+            //Model.botClient = OV2Bot;
+            //OV2Bot.StartReceiving();
+           
+            //Console.ReadLine();
 
         }
 
